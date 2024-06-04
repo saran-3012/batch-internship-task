630bf620184b5e147b8be75abcce9fe22201cef6
Saran Kumar S <sarankumar.95977@gmail.com>
2024-06-04 20:30:34 +0530

    Scroll Animation Added

--- main.js
+++ main.js
@@ -17,3 +17,28 @@ navbarMenuItems.forEach((menuItem, index) => {
     menuItem.addEventListener('click', () => setActiveMenuItem(index));
 });
 
+// Slide Animation 
+
+const containers = document.querySelectorAll('.container');
+const totalContainers  = containers.length;
+let visitedContainers = new Array(totalContainers).fill(false);
+
+const isVisibleOnScreen = (element) => {
+    const {top, left, bottom, right} = element.getBoundingClientRect();
+    const {innerHeight, innerWidth} = window;
+    return ((top > 0 && top < innerHeight) || (bottom > 0 && bottom < innerHeight)) && ((left > 0 && left < innerWidth) || (right > 0 && right < innerWidth));
+};
+
+const checkContainers = () => {
+    containers.forEach((container, index) => {
+        if (!visitedContainers[index] && isVisibleOnScreen(container)) {
+            setTimeout(() => {
+                container.classList.add('visited');
+                visitedContainers[index] = true;
+            }, 100);
+        }
+    });
+};
+
+document.addEventListener('DOMContentLoaded', checkContainers);
+document.addEventListener('scroll', checkContainers);
